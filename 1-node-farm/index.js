0348e897d5f1af9732c3910a6b5f16e10ed681ed
Sameer <sameer@theastrotime.com>
2025-11-29 23:29:00 +0530

    learn read and write file asynchronousll

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// -------------- Blocking synchronous way ----------
+
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
 console.log('textIn', textIn)
@@ -7,3 +9,20 @@ console.log('textIn', textIn)
 const textOut = `This is what we know about the avacado:  ${textIn}.\nCreated on ${Date.now()}`
 
 fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
+
+// -------------- Non-Blocking asynchronous way ----------
+
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    console.log(data1)
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2)=>{
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written');
+            });
+        })
+    })
+});
+
+console.log('Will read file')
