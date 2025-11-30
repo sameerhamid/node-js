fb0b18ae938fab3e0a1454b7289b81beb9886212
Sameer <sameer@theastrotime.com>
2025-11-30 11:37:37 +0530

    learn create an http server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,28 +1,41 @@
 const fs = require('fs');
+const http = require('http')
+
+// ----------------- FILES ------------------
 
 // -------------- Blocking synchronous way ----------
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log('textIn', textIn)
+// console.log('textIn', textIn)
 
-const textOut = `This is what we know about the avacado:  ${textIn}.\nCreated on ${Date.now()}`
+// const textOut = `This is what we know about the avacado:  ${textIn}.\nCreated on ${Date.now()}`
 
-fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
+// fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
 
 // -------------- Non-Blocking asynchronous way ----------
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    console.log(data1)
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2)=>{
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written');
-            });
-        })
-    })
-});
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     console.log(data1)
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2)=>{
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written');
+//             });
+//         })
+//     })
+// });
 
-console.log('Will read file')
+// console.log('Will read file')
+
+// ----------------------- SERVER -----------------
+
+const server = http.createServer((req, res)=>{
+    res.end("Hello from the server!");
+})
+
+server.listen(9600, '127.0.0.1', ()=>{
+    console.log("Server listening on port 9600");
+})
