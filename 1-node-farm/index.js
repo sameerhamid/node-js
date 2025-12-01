161d84d9c24605cfdcd8cba2f9837bbf1b542fa6
Sameer <sameer@theastrotime.com>
2025-12-01 10:45:38 +0530

    Parse Variables from URLs

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -60,7 +60,7 @@ const replaceTemplate = (temp, product) => {
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%NUTIRENTS%}/g, product.nutrients);
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
@@ -81,10 +81,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const parsedData = JSON.parse(data);
 
 const server = http.createServer((req, res)=>{
-    console.log(req.url)
-    const pathName = req.url;
+
+    const {query, pathname} = url.parse(req.url, true)
     // ---------- overview page -------------
-    if(pathName === '/' || pathName === '/overview') {
+    if(pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {"Content-type": "text/html"});
 
         const cardsHtml = parsedData.map(el => replaceTemplate(tempCard, el)).join('');
@@ -92,11 +92,15 @@ const server = http.createServer((req, res)=>{
         res.end(output);
 
         // ---------- product page -------------
-    }else if(pathName === '/product') {
-        res.end('This is the PRODUCT');
+    }else if(pathname === '/product') {
+        res.writeHead(200, {"Content-type": "text/html"});
+        const product = parsedData[+query.id];
+        const output = replaceTemplate(tempProduct, product)
+
+        res.end(output);
 
         // ---------- API -------------
-    }else if(pathName ===`/api`) {
+    }else if(pathname ===`/api`) {
         res.end(data);
 
         // ---------- Not Found -------------
