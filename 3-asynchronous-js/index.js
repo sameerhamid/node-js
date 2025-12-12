44ee2c29fd319f94897814add239cdff91ff197a
Sameer <sameer@theastrotime.com>
2025-12-12 11:45:05 +0530

    learn Building Promises

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -13,16 +13,47 @@ const superagent = require('superagent');
 //     });
 // })
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data)=>{
-    console.log("data>>", data.toString())
-    // From Callback Hell to Promises
-    superagent.get(`https://dog.ceo/api/breed/${data.toString()}/images/random`)
-    .then((res)=>{
-        console.log("result>>>", JSON.stringify(res.body.message));
-        fs.writeFile(`${__dirname}/dog-image.txt`, res.body.message, (err)=>{
-            if(err) console.log('error>>>', err)
+
+// fs.readFile(`${__dirname}/dog.txt`, (err, data)=>{
+//     console.log("data>>", data.toString())
+//     // From Callback Hell to Promises
+//     superagent.get(`https://dog.ceo/api/breed/${data.toString()}/images/random`)
+//     .then((res)=>{
+//         console.log("result>>>", JSON.stringify(res.body.message));
+//         fs.writeFile(`${__dirname}/dog-image.txt`, res.body.message, (err)=>{
+//             if(err) console.log('error>>>', err)
+//         });
+//     }).catch(err =>{
+//         console.log(err.message);
+//     });
+// })
+
+
+
+const readFilePro = (file)=>{
+    return new Promise((resolve, reject)=>{
+        fs.readFile(file, (err, data)=>{
+            if(err) reject(err);
+            resolve(data);
+        })
+    })
+}
+
+const writeFilePro = (file, data)=>{
+    return new Promise((resolve, reject)=>{
+        fs.writeFile(file, data, (err)=>{
+            if(err) reject(err);
+            resolve('Data written in the file successfull!');
         });
-    }).catch(err =>{
-        console.log(err.message);
-    });
+    })
+}
+
+readFilePro(`${__dirname}/dog.txt`).then(data => {
+    console.log(data.toString());
+    return superagent.get(`https://dog.ceo/api/breed/${data.toString()}/images/random`)
+}).then((res) => {
+    console.log("result>>>", JSON.stringify(res.body.message));
+    return writeFilePro(`${__dirname}/dog-image.txt`, res.body.message)
+}).then(data => { console.log(data) }).catch(err => {
+    console.log(err)
 })
