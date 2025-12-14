6c55f90eb3122fdf87f43852ad57d1b1e9ee3b2d
Sameer <sameer@theastrotime.com>
2025-12-14 15:48:15 +0530

    Handling POST Requests

--- 4-natours/app.ts
+++ 4-natours/app.ts
@@ -16,6 +16,21 @@ app.get('/api/v1/tours', (req, res)=>{
     })
 })
 
+app.post('/api/v1/tours', (req, res)=>{
+    console.log(req.body);
+    const newId = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body)
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),  (err)=>{
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
