9c96eeaf1dfaf59ede33a061001c186fa69b36ad
Sameer <sameer@theastrotime.com>
2025-12-21 15:15:56 +0530

    Connecting Our Database with the Express App

--- 4-natours/server.ts
+++ 4-natours/server.ts
@@ -1,9 +1,16 @@
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config({ path: './config.env' });
 import app from './app';
 
 
 const PORT = process.env.PORT || 3000;
+const MONOGDB_URL = process.env.MONOGDB_URL ?? "";
+
+mongoose.connect(MONOGDB_URL).then(_con => {
+    // console.log(con.connections);
+    console.log("DB connection successful!");
+});
 
 // console.log(process.env)
 
