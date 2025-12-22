05bcc8a52ab287ed342fa06e58dd9f2377880ad9
Sameer <sameer@theastrotime.com>
2025-12-22 16:01:11 +0530

    Updating Documents

--- 4-natours/src/controllers/tourController.ts
+++ 4-natours/src/controllers/tourController.ts
@@ -55,34 +55,36 @@ const createTour = async (req: any, res: any) => {
 	}
 }
 
-const updateTour = (req: any, res: any) => {
-	// const tour = tours.find((el: any) => el.id === +req.params.id)
-	// if (!tour) {
-	//     return res.status(404).json({
-	//         status: 'failed',
-	//         message: 'Invalid ID',
-	//     })
-	// }
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tours: '<Tour Update Successfull!>'
-		}
-	})
+const updateTour = async (req: any, res: any) => {
+	try {
+		const newTour = await Tour.findByIdAndUpdate(req.body, { new: true });
+		res.status(201).json({
+			status: 'success',
+			data: {
+				tour: newTour
+			}
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: 'fail ',
+			message: error,
+		})
+	}
 }
 
-const deletTour = (req: any, res: any) => {
-	// const tour = tours.find((el: any) => el.id === +req.params.id)
-	// if (!tour) {
-	//     return res.status(404).json({
-	//         status: 'failed',
-	//         message: 'Invalid ID',
-	//     })
-	// }
-	res.status(204).json({
-		status: 'success',
-		data: null
-	})
+const deletTour = async (req: any, res: any) => {
+	try {
+		const tour = Tour.findByIdAndDelete(req.params.id, { new: true });
+		res.status(204).json({
+			status: 'success',
+			data: tour
+		})
+	} catch (error) {
+		res.status(400).json({
+			status: 'fail ',
+			message: error,
+		})
+	}
 }
 
 export { getAllTours, getTour, createTour, updateTour, deletTour }
