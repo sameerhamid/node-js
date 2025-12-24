510ef6438b172fd11eaf3db3547f73c8d99cff5a
Sameer <sameer@theastrotime.com>
2025-12-24 12:42:56 +0530

    Modelling the Tours

--- 4-natours/src/models/tourModel.ts
+++ 4-natours/src/models/tourModel.ts
@@ -10,10 +10,45 @@ const tourShema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a price'],
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5,
-    }
+    },
+    ratingsQuanitity: {
+        type: Number,
+        default: 0
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a summary'],
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: true
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+    },
+    startDate: [Date],
 }, { timestamps: true });
 
 const Tour = mongoose.model('Tour', tourShema);
