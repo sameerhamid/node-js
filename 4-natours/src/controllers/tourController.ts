f4ecfadf452ce425759f2ff473203e06b426239c
Sameer <sameer@theastrotime.com>
2025-12-27 11:59:59 +0530

    add Aggregation Pipeline: Matching and Grouping

--- 4-natours/src/controllers/tourController.ts
+++ 4-natours/src/controllers/tourController.ts
@@ -96,4 +96,41 @@ const deletTour = async (req: any, res: any) => {
 	}
 }
 
-export { getAllTours, getTour, createTour, updateTour, deletTour, aliasTopTours }
+const getTourStats = async (req: any, res: any) => {
+	try {
+		const stats = await Tour.aggregate([
+			{
+				$match: { ratingsAverage: { $gte: 4.5 } }
+			},
+			{
+				$group: {
+					// _id: '$ratingsAverage',
+					_id: { $toUpper : '$difficulty'},
+					numTours: { $sum: 1 },
+					numRatings: { $sum: '$ratingsQuantity' },
+					avgRatings: { $avg: '$ratingsAverage' },
+					avgPrice: { $avg: '$price' },
+					minPrice: { $min: '$price' },
+					macPrice: { $max: '$price' }
+				},
+			},
+			{
+				$sort: { avgPrice: 1 }
+			},
+			// {
+			// 	$match: { _id: { $ne: 'EASY' }}
+			// }
+		]);
+		res.status(200).json({
+			status: 'success',
+			data: { stats },
+		})
+	} catch (error) {
+		res.status(400).json({
+			status: 'fail ',
+			message: error,
+		})
+	}
+}
+
+export { getAllTours, getTour, createTour, updateTour, deletTour, aliasTopTours, getTourStats }
--- 4-natours/src/routes/tourRoutes.ts
+++ 4-natours/src/routes/tourRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import {getAllTours, createTour, getTour, updateTour, deletTour, aliasTopTours} from '../controllers/tourController'
+import {getAllTours, createTour, getTour, updateTour, deletTour, aliasTopTours, getTourStats} from '../controllers/tourController'
 
 const router = express.Router()
 
 // router.param('id', checkID)
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/stats').get(getTourStats);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deletTour);
