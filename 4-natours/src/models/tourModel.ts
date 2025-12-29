b1a7c6eb95382b05d32f5ac66b321e51819a54d1
Sameer <sameer@theastrotime.com>
2025-12-29 12:00:04 +0530

    add Data Validation: Built-In Validators

--- 4-natours/src/models/tourModel.ts
+++ 4-natours/src/models/tourModel.ts
@@ -6,6 +6,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal than 40 chars'],
+        minlength: [10 , 'A tour name must have more  or equal than 3 chars'],
     },
     slug: {
         type: String,
@@ -24,11 +27,14 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: { values: ["easy", "medium", "difficult"], message: 'Difficulty is either: easy, medium, difficult' }
     },
     ratingsAverage: {
         type: Number,
         default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be bellow 5.0']
     },
     ratingsQuanitity: {
         type: Number,
