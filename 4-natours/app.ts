f034ff125fcde727cc9f61c8d8f3e6455b2f33f6
Sameer <sameer@theastrotime.com>
2025-12-30 12:12:23 +0530

    Handle Unhandled Routes

--- 4-natours/app.ts
+++ 4-natours/app.ts
@@ -15,10 +15,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-    console.log("Hello from the middleware");
-    next();
-})
 
 app.use((req: any, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -30,5 +26,12 @@ app.use((req: any, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.use((req, res, next)=>{
+    res.status(404).json({
+        status: 'failed',
+        message: `Can't find ${req.originalUrl} on this server!`
+    })
+    next();
+})
 
 export default app;
