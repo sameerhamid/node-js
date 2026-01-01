5dcfffe385d2b78eab5dd909a5ced30efadd6dab
Sameer <sameer@theastrotime.com>
2026-01-01 13:28:46 +0530

    Errors During Development vs Production

--- 4-natours/src/controllers/errorController.ts
+++ 4-natours/src/controllers/errorController.ts
@@ -1,15 +1,45 @@
 import { Request, Response, NextFunction } from 'express';
 import { AppError } from '../utils/appError';
 
+const sendErrorForDev = (err: AppError, res: Response) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+}
+
+const sendErrorForProd = (err: AppError, res: Response) => {
+    // OPERATION ERROR, trusted errror: send message to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+
+    // PROGRAMING OR OTHER UNKNOWN ERRROR: don't want to leak error details
+    } else {
+        // 1. LOG ERROR
+        console.error('ERROR 🔥', err);
+
+        // 2. SEND GENERIC MESSAGE
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        })
+    }
+}
 
 const errorController = (err: AppError, req: Request, res: Response, next: NextFunction) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error'
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
-    next();
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorForDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorForProd(err, res);
+        next();
+    }
 }
 
-export default errorController
+export default errorController;
