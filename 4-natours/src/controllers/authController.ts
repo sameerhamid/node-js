faf1a93948e9965e07b2249b8d215224ca63a2f1
Sameer <sameer@theastrotime.com>
2026-01-05 16:32:52 +0530

    fix signup

--- 4-natours/src/controllers/authController.ts
+++ 4-natours/src/controllers/authController.ts
@@ -4,7 +4,12 @@ import catchAsync from "../utils/catchAsync";
 
 
 const signUp = catchAsync(async (req: any, res: any, next: NextFunction) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    });
 
     res.status(201).json({
         status: 'success',
