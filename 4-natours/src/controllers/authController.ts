e3a2b5bd6b4e8be5a57335d3439fc159ec2a6938
Sameer <sameer@theastrotime.com>
2026-01-05 16:56:55 +0530

    send jwt token when sign up

--- 4-natours/src/controllers/authController.ts
+++ 4-natours/src/controllers/authController.ts
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken'
 import { NextFunction } from "express";
 import User from "../models/userModel";
 import catchAsync from "../utils/catchAsync";
@@ -11,12 +12,17 @@ const signUp = catchAsync(async (req: any, res: any, next: NextFunction) => {
         confirmPassword: req.body.confirmPassword
     });
 
+    const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET!, {
+        expiresIn: '90d',
+    });
+
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
-    })
+    });
 });
 
 
