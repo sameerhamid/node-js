5c81d553f09596588acb7420094c92815bdbbfb0
Sameer <sameer@theastrotime.com>
2026-01-10 18:52:55 +0530

    Set Security HTTP Headers

--- 4-natours/app.ts
+++ 4-natours/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
-import reateLimit from 'express-rate-limit'
+import reateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import tourRouter from './src/routes/tourRoutes'
 import userRouter from './src/routes/userRoutes'
 import { AppError } from './src/utils/appError';
@@ -11,22 +12,31 @@ const PORT = 3000;
 app.set('query parser', 'extended');
 // 1) -------------- MIDDLEWARES ----------
 
+// Set Security http headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = reateLimit({
-    max: 1,
+    max: 100,
     windowMs: 60 * 60 * 1000,
     message: 'Too many request from this IP, please try again in an hour',
 });
 
 app.use('/api', limiter)
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 
+// Test middleware
 app.use((req: any, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
