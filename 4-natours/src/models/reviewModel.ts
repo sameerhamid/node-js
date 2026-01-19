f9797ab7fb5085ce22d98b70a6fc122939428d5a
Sameer <sameer@theastrotime.com>
2026-01-19 11:59:05 +0530

    Calcule Average Rating on Tours

--- 4-natours/src/models/reviewModel.ts
+++ 4-natours/src/models/reviewModel.ts
@@ -1,11 +1,25 @@
-import mongoose, { Query } from "mongoose";
+import mongoose, { Model, Query, Types } from "mongoose";
+import Tour from "./tourModel";
+
+/** Review document */
+interface IReview {
+  review: string;
+  rating: number;
+  tour: Types.ObjectId;
+  user: Types.ObjectId;
+}
+
+/** Review model (statics go here) */
+interface ReviewModel extends Model<IReview> {
+  calAverageRatings(tourId: Types.ObjectId): Promise<void>;
+}
 
 const reviewSchema = new mongoose.Schema({
     review: {
         type: String,
         required: true,
         trim: true,
-        minLength: [3, 'A review must have more or equal than 3 chars'],
+        minLength: [2, 'A review must have more or equal than 2 chars'],
     },
     rating: {
         type: Number,
@@ -35,6 +49,31 @@ reviewSchema.pre(/^find/, function (this: Query<any, any>) {
     })
 });
 
-const Review = mongoose.model('Review', reviewSchema);
+reviewSchema.statics.calAverageRatings = async function (tourId: string) {
+    const statts = await this.aggregate([
+        {
+            $match: { tour: tourId },
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRatings: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingsAverage: statts[0].avgRating,
+        ratingsQuanitity: statts[0].nRatings,
+    })
+}
+
+reviewSchema.post('save', function(){
+    // this points to current review
+    const ReviewModel = this.constructor as ReviewModel;
+    ReviewModel.calAverageRatings(this.tour);
+})
+
+const Review = mongoose.model<IReview, ReviewModel>('Review', reviewSchema);
 
 export default Review;
