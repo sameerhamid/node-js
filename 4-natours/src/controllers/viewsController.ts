63069570f77793a42c3630c5ac1d859b6ff230e3
Sameer <sameer@theastrotime.com>
2026-01-20 15:53:26 +0530

    WIP: Building the Tour Overview

--- 4-natours/src/controllers/viewsController.ts
+++ 4-natours/src/controllers/viewsController.ts
@@ -1,9 +1,17 @@
+import Tour from "../models/tourModel";
+import catchAsync from "../utils/catchAsync";
 
-const getOverview = (req: any, res: any) => {
+const getOverview = catchAsync(async (req: any, res: any) => {
+    // 1) Get tour data from collection
+    const tours = await Tour.find();
+
+    // 2) Build template
+    // 3) Render that template using tour data from 1)
     res.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     });
-}
+})
 
 const getTour = (req: any, res: any) => {
     res.status(200).render('tour', {
