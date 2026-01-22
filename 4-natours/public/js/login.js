6acccf9517ff27156694660043bd9d29e6f17903
Sameer <sameer@theastrotime.com>
2026-01-22 14:30:25 +0530

    add login page

---
+++ 4-natours/public/js/login.js
@@ -0,0 +1,39 @@
+const login = async (email, password) =>{
+    if (!email || !password) {
+        return;
+    }
+    try {
+        // const res = await axios({
+        //     method: 'POST',
+        //     url: 'http://127.0.0.1:3000/api/v1/users/login',
+        //     body: {
+        //         email: email,
+        //         password: password
+        //     }
+        // });
+        const res = await fetch('/api/v1/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin',
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await res.json(); // 👈 REQUIRED
+
+        console.log("response>>>>>", data);
+    } catch (error) {
+        console.log('error>>>>>', error)
+    }
+
+}
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password)
+})
+
+console.log("login form>>>")
