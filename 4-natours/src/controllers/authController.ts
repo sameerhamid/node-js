0eece5a8e3b389ccad4affd6e6b3f0b5d7c8ebea
Sameer <sameer@theastrotime.com>
2026-01-22 14:21:54 +0530

    verify token through cookie

--- 4-natours/src/controllers/authController.ts
+++ 4-natours/src/controllers/authController.ts
@@ -70,6 +70,8 @@ const verfiyToken = catchAsync(async (req: any, res: any, next: NextFunction) =>
     let token;
     if(req?.headers?.authorization && req.headers.authorization.startsWith('Bearer') ){
         token = req.headers.authorization.split(' ')[1];
+    }else if(req?.cookies?.jwt){
+        token = req.cookies.jwt;
     }
     if(!token){
         return next(new AppError('You are not logged in! Please login to get access.', 401));
