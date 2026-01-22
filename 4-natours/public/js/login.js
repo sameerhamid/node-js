5a6a1e569c9b7291d91d604a1c476f1b9240a92c
Sameer <sameer@theastrotime.com>
2026-01-22 17:46:42 +0530

    show custom alert

--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -1,43 +1,29 @@
-const login = async (email, password) =>{
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const login = async (email, password) => {
     if (!email || !password) {
         return;
     }
     try {
-        // const res = await axios({
-        //     method: 'POST',
-        //     url: 'http://127.0.0.1:3000/api/v1/users/login',
-        //     body: {
-        //         email: email,
-        //         password: password
-        //     }
-        // });
-        const res = await fetch('/api/v1/users/login', {
+        const res = await axios({
             method: 'POST',
+            url: '/api/v1/users/login',
+            data: {
+                email,
+                password
+            },
             headers: {
                 'Content-Type': 'application/json'
-            },
-            credentials: 'same-origin',
-            body: JSON.stringify({ email, password })
+            }
         });
-
-        const data = await res.json(); // 👈 REQUIRED
-        if(data?.status === 'success'){
-            alert('Logged in successfull!');
+        if (res?.data?.status === 'success') {
+            showAlert('success', 'Logged in successfull!');
             window.setTimeout(() => {
                 location.assign('/');
             }, 1000);
         }
     } catch (error) {
-        alert(error?.response?.data.message ?? "Error while logging user");
+        showAlert("error", error?.response?.data.message ?? "Error while logging user");
     }
-
 }
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    login(email, password)
-})
-
-console.log("login form>>>")
