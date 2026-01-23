5354f3164d0b7bea2592f4dffae98816505c605e
Sameer <sameer@theastrotime.com>
2026-01-23 12:42:42 +0530

    handle error for render pages

--- 4-natours/src/controllers/errorController.ts
+++ 4-natours/src/controllers/errorController.ts
@@ -17,13 +17,22 @@ const handleValidationErrorDB = (err: any) => {
     return new AppError(message, 400);
 }
 
-const sendErrorForDev = (err: AppError, res: Response) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack
+const sendErrorForDev = (err: AppError, req: Request, res: Response) => {
+    // API
+    if (req?.originalUrl?.startsWith('/api')) {
+        return res.status(err.statusCode).json({
+            status: err.status,
+            error: err,
+            message: err.message,
+            stack: err.stack
+        })
+    }
+    // RENDERED WEBSITE
+    return res.status(err.statusCode).render('error', {
+        title: 'Somethig went wrong',
+        msg: err?.message ?? ""
     })
+
 }
 
 const handleJwtError = (err: any) => new AppError('Invalid token. Please login again!', 401)
@@ -31,24 +40,34 @@ const handleJwtError = (err: any) => new AppError('Invalid token. Please login a
 const handleJwtExpiredError = (err: any) => new AppError('Your token has expired! Please login again.', 401);
 
 const sendErrorForProd = (err: any, res: Response) => {
-    // OPERATION ERROR, trusted errror: send message to client
-    if (err.isOperational) {
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message,
-        });
 
-    // PROGRAMING OR OTHER UNKNOWN ERRROR: don't want to leak error details
-    } else {
-        // 1. LOG ERROR
-        console.error('ERROR 🔥', err);
+    // API
+    if (err?.originalUrl?.startsWith('/api')) {
+        // OPERATION ERROR, trusted errror: send message to client
+        if (err.isOperational) {
+            return res.status(err.statusCode).json({
+                status: err.status,
+                message: err.message,
+            });
 
-        // 2. SEND GENERIC MESSAGE
-        res.status(500).json({
-            status: 'error',
-            message: 'Something went very wrong!'
-        })
+            // PROGRAMING OR OTHER UNKNOWN ERRROR: don't want to leak error details
+        } else {
+            // 1. LOG ERROR
+            console.error('ERROR 🔥', err);
+
+            // 2. SEND GENERIC MESSAGE
+            return res.status(500).json({
+                status: 'error',
+                message: 'Something went very wrong!'
+            })
+        }
     }
+    // RENDERED WEBSITE
+    return res.status(err.statusCode).render('error', {
+        title: 'Somethig went wrong',
+        msg: "Please try agian later."
+    })
+
 }
 
 const errorController = (err: any, req: Request, res: Response, next: NextFunction) => {
@@ -56,12 +75,11 @@ const errorController = (err: any, req: Request, res: Response, next: NextFuncti
     err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
-        sendErrorForDev(err, res);
+        sendErrorForDev(err, req ,res);
     }
 
     if (process.env.NODE_ENV === 'production') {
         let error = {...err};
-
         error.message = err.message;
         error.name = err.name;
 
